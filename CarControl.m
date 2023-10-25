f9b374250fd2e24a90a50825043cc6bc6e62743f
extra-ordinary <66090552+extra-ordinary@users.noreply.github.com>
2023-10-25 08:11:41 -0700

    Update CarControl.m
    A little more functionality, can't really proceed until design is fully finished.

--- CarControl.m
+++ CarControl.m
@@ -11,15 +11,19 @@ ultrasonic_sensor = 2;
 touch_sensor = 3;
 % % A is the left motor
 % % D is the right motor
+% % C is the claw motor
 
 % Booleans and States
 manual_control = false;
 yellow_found = false;
 car_state = 0;
+threshold = 50; % Distance threshold for car to act upon
 
 % Timers
-timer_reverse = 1;
-timer_turn = 1;
+timer_reverse = 0;
+timer_turn = 0;
+reverse_time = 1;
+turn_time = 1;
 
 % Color Sensor setup
 brick.setColorMode(color_sensor, 2);
@@ -29,7 +33,7 @@ brick.setColorMode(color_sensor, 2);
 % % Red = 5
 
 % Keyboard control setup
-global key;
+global key; %#ok<GVMIS> %% blocks warning message
 InitKeyboard();
 
 % Automatic Navigation
@@ -38,7 +42,7 @@ while 1
     switch car_state
 
         case -1 % Manual control
-            disp("Manual control activated");
+            disp("!!! Manual control activated !!!");
             switch key
                 case 'w' % Move forward
                     brick.MoveMotor('AD', speed_forward);
@@ -52,39 +56,43 @@ while 1
                     brick.StopMotor('A');
                 case 'backspace'
                     brick.StopMotor('AD');
-  
+                case 'c'
+                    brick.MoveMotor('C', speed_claw_open);
+                case 'z' 
+                    brick.MoveMotor('C', speed_claw_close);
+                case 'p'
+                    break;
+            end % key switch end
+            
         case 0 % Move forward and sense
             brick.MoveMotor('AD', speed_forward); % Automatically moves forward
-    
-            % Ultrasonic Sensor
+  
             distance = brick.UltrasonicDist(ultrasonic_sensor);
-            % % May need to implement more for the ultrasonic sensor but idk
-            
-            % Color Sensor 
             color = brick.ColorCode(color_sensor);
-            if(color == 5) % Color is Red
-                disp("Red detected."); % This might infinite loop the car because of distance
-                car_state = 1;
-            end
+            touched = brick.TouchPressed(touch_sensor);
             
-            if(color == 4 && yellow_found == false) % Color is Yellow and the passenger HASN'T been picked up
+            if(color == 5) % Color is Red
+                disp("Red detected.");
+                car_state = 1;
+
+            elseif(color == 4 && yellow_found == false) % Color is Yellow and the passenger HASN'T been picked up
                 yellow_found = true;
                 brick.StopMotors('AD');
                 disp("Yellow detected.");
                 car_state = -1;
-            end
-            
-            if(color == 2 && yellow_found == true) % Color is Blue and the passenger HAS been picked up
+
+            elseif(color == 2 && yellow_found == true) % Color is Blue and the passenger HAS been picked up
                 brick.StopMotors('AD');
                 disp("Blue detected.");
                 car_state = -1;
             end
 
-            % Touch Sensor
-            if(brick.TouchPressed(touch_sensor))
+            if(touched)
                 disp("Touch was sensed.");
                 car_state = 2;
             end
+
+        
 
         case 1 % Red detected case
             brick.StopMotor('AD');
@@ -93,18 +101,41 @@ while 1
 
         case 2 % Touch sensed case
             brick.MoveMotor('AD', speed_backwards); % Back away from the wall
-            pause(timer_reverse);
-            brick.MoveMotor('D', speed_turnspeed); % Turn right
-            brick.StopMotor('A'); % Turn right
-            pause(timer_turn);
-            car_state = 0;
+            timer_reverse = tic;
+            if(toc(timer_reverse) >= reverse_time)
+                brick.MoveMotor('D', speed_turnspeed); % Turn right
+                brick.StopMotor('A'); % Turn right
+                timer_turn = tic;
+                if(toc(timer_turn) >= turn_timer)
+                    car_state = 0;
+                    timer_reverse = 0;
+                    timer_turn = 0;
+                end
+            end
+        
+        case 3 % Ultrasonic sensor - wall close case
+            brick.MoveMotor('AD', speed_backwards); % Back away from the wall
+            timer_reverse = tic;
+            if(toc(timer_reverse) >= reverse_time)
+                brick.MoveMotor('D', speed_turnspeed); % Turn right
+                brick.StopMotor('A'); % Turn right
+                timer_turn = tic;
+                if(toc(timer_turn) >= turn_timer)
+                    car_state = 0;
+                    timer_reverse = 0;
+                    timer_turn = 0;
+                end
+            end
+
+            
             
         
-    end
+    end % car_state switch end
+     
     if(key == 'p')
-        brick.StopMotors('AD');
+        brick.StopMotors('ACD');
         break;
     end
-    
-end
+
+end % while loop end
 CloseKeyboard();
